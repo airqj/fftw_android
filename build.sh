d2321abdfce453f4a5990b183dc30a6af476a8c3
airqj <gugeqinjianbo@gmail.com>
2017-11-20 21:33:54 +0800

    modify build.sh

--- build.sh
+++ build.sh
@@ -4,27 +4,30 @@
 # Make sure you have NDK_DIR defined in .bashrc or .bash_profile
 
 #NDK Version r9c  http://dl.google.com/android/ndk/android-ndk-r9c-linux-x86_64.tar.bz2
-NDK_DIR="/home/hekai/software/android-ndk-r9c"
+NDK_DIR="/home/qjb/Downloads/android-ndk-r15"
 INSTALL_DIR="`pwd`/jni/fftw3"
-SRC_DIR="`pwd`/../fftw-3.3.3"
+SRC_DIR="`pwd`/../fftw-3.3.7"
 
 cd $SRC_DIR
 
-export PATH="$NDK_DIR/toolchains/arm-linux-androideabi-4.8/prebuilt/linux-x86_64/bin/:$PATH"
-export SYS_ROOT="$NDK_DIR/platforms/android-8/arch-arm/"
-export CC="arm-linux-androideabi-gcc --sysroot=$SYS_ROOT -march=armv7-a -mfloat-abi=softfp"
-export LD="arm-linux-androideabi-ld"
-export AR="arm-linux-androideabi-ar"
-export RANLIB="arm-linux-androideabi-ranlib"
-export STRIP="arm-linux-androideabi-strip"
+export PATH="$NDK_DIR/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/bin:$PATH"
+#export SYS_ROOT="$NDK_DIR/platforms/android-8/arch-arm/"
+export SYS_ROOT="$NDK_DIR/platforms/android-26/arch-arm64/"
+#export CC="aarch64-linux-android-gcc --sysroot=$SYS_ROOT -march=armv8-a -mfloat-abi=softfp"
+export CC="aarch64-linux-android-gcc --sysroot=$SYS_ROOT -march=armv8-a"
+export LD="aarch64-linux-android-ld"
+export AR="aarch64-linux-android-ar"
+export RANLIB="aarch64-linux-android-ranlib"
+export STRIP="aarch64-linux-android-strip"
 #export CFLAGS="-mfpu=neon -mfloat-abi=softfp"
 
 mkdir -p $INSTALL_DIR
-./configure --host=arm-eabi \
+#./configure --host=arm-eabi \
+./configure --host=aarch64 \
 	--prefix=$INSTALL_DIR \
 	LIBS="-lc -lgcc" \
 	--enable-float \
-	--enable-threads \
+#	--enable-threads \
 #	--with-combined-threads \
 	--enable-neon
 
